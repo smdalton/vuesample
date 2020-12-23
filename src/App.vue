70a41288f2d2d615fea61adb7ee23f81d61d8ccd
smd <shanemdalton@gmail.com>
2020-12-22 18:19:44 -0800

    Event emissions handled correctly

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,57 @@
 
   <div id="app" class="small-container">
     <h1>Employees All </h1>
-    <EmployeeTable />
+    <employee-form @add:employee="addEmployee" @cat:message="catMessageAlert" />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 export default {
   name: 'App',
   components: {
     EmployeeTable,
-  }
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Frodo baggins',
+          email: 'ringbearer@mordorexcursions.com'
+        },
+        {
+          id: 2,
+          name: 'Gandalf the Grey',
+          email: 'gandalf@icandomagic.com'
+        },
+        {
+          id: 3,
+          name: 'Samwise',
+          email: 'sam@ringsupport.com'
+        },
+      ],
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0 ? 
+        this.employees[this.employees.length -1].id : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id};
+      
+      this.employees = [...this.employees, newEmployee]
+      console.log(this.employees)
+    },
+    catMessageAlert(){
+      alert('Mew Mew fish time')
+    },
+  },
 }
 </script>
 
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="employee-form">
+      <!-- Event Handler for form submissions, inject database calls at it's definition -->
+    <form @submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input v-model="employee.name" type="text" />
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="text" />
+      <button>Add Employee</button>
+    </form>
+    <button @click="catMessage">The cat says:</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+    methods: {
+// TODO: do post/get here when database is integrated
+    handleSubmit() {
+      this.$emit('add:employee', this.employee)
+      },
+      catMessage() {
+        this.$emit('cat:message')
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
